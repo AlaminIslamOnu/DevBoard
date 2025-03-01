9598e22ad3afa9f67f0ab31f1a6a048fbdde8909
AlaminIslamOnu <alamin.ndc.17@gmail.com>
2025-03-01 10:14:11 +0600

    added every layout

---
+++ script/function.js
@@ -0,0 +1,15 @@
+document.getElementById("banner").addEventListener("click",function(){ 
+   window.location.href="./blog.html"
+
+})
+document.getElementById("homepage").addEventListener("click",function(){ 
+   window.location.href="./index.html"
+// console.log('clicked');
+
+
+})
+
+
+
+
+
